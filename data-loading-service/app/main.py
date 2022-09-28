f04f0d8b0a526845c8c5ec131f1c4996884445d9
albertkun <albertk@gmx.com>
2022-09-28 15:58:36 -0700

    feat: separated data service from `fastapi` feat: scheduler to run gtfs updates every 20 secs feat: scheduler to run ftp updates every 15 mins

--- data-loading-service/app/main.py
+++ data-loading-service/app/main.py
@@ -1,30 +1,49 @@
 from config import Config
 import update_canceled_trips as update_canceled_trips
-
+import utils.gtfs_rt_helper as gtfs_rt_helper
 import threading
 import time
-import schedule
+from schedule import every, repeat, run_pending
+# import schedule
 
-UPDATE_INTERVAL = 30
+# UPDATE_INTERVAL = 5
 
-def run_continuously(interval=UPDATE_INTERVAL):
-    cease_continuous_run = threading.Event()
-    class ScheduleThread(threading.Thread):
-        @classmethod
-        def run(cls):
-            while not cease_continuous_run.is_set():
-                schedule.run_pending()
-                time.sleep(interval)
-    continuous_thread = ScheduleThread()
-    continuous_thread.start()
-    return cease_continuous_run
+# def run_continuously(interval=UPDATE_INTERVAL):
+#     cease_continuous_run = threading.Event()
+#     class ScheduleThread(threading.Thread):
+#         @classmethod
+#         def run(cls):
+#             while not cease_continuous_run.is_set():
+#                 schedule.run_pending()
+#                 time.sleep(interval)
+#     continuous_thread = ScheduleThread()
+#     continuous_thread.start()
+#     return cease_continuous_run
 
-def background_job():
+@repeat(every(20).seconds)
+def gtfs_rt_scheduler():
+    gtfs_rt_helper.update_gtfs_realtime_data()
+
+@repeat(every(15).minutes)
+def canceled_trips_update_scheduler():
     update_canceled_trips.run_update()
 
-
+def initial_data_loading():
+    print('initial run started at ' + str(time.time()))
+    update_canceled_trips.run_update()
+    gtfs_rt_helper.update_gtfs_realtime_data()
+        
 if __name__ == '__main__':
-    schedule.every().second.do(background_job)
-    stop_run_continuously = run_continuously()
+    initial_run()
+    while True:
+        run_pending()
+    # schedule.every().second.do(background_job('canceled_trips'))
+    # schedule.every().second.do(test1)
+    # schedule.every().second.do(test1)
+    # schedule.every().second.do(background_job)
+    # # background_job('canceled_trips')
+    # # background_job('gtfs_rt')
+    # stop_run_continuously = run_continuously()
+    # schedule.every().second.do(background_job)
     # update_canceled_trips.run_update()
     
