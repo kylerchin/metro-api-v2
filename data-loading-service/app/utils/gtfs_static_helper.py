6f96e63b3f6daefe5afbd968d88a67a041936917
albertkun <albertk@gmx.com>
2022-10-20 16:26:55 -0700

    fix: [data-loading] oops, forgot `/` for bus_file_path

--- data-loading-service/app/utils/gtfs_static_helper.py
+++ data-loading-service/app/utils/gtfs_static_helper.py
@@ -26,7 +26,7 @@ def update_calendar_dates():
 
 def update_gtfs_static_files():
     for file in list_of_gtfs_static_files:
-        bus_file_path = pd.read_csv("https://gitlab.com/LACMTA/gtfs_bus/-/raw/master" + file + '.txt')
+        bus_file_path = pd.read_csv("https://gitlab.com/LACMTA/gtfs_bus/-/raw/master/" + file + '.txt')
         rail_file_path = pd.read_csv("https://gitlab.com/LACMTA/gtfs_rail/-/raw/master/" + file + '.txt')
         temp_df_bus = pd.read_csv(bus_file_path)
         temp_df_bus['agency_id'] = 'LACMTA'
