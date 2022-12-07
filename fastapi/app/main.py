fd994142ca8315bbe3422890f46109018c557387
albertkun <albertk@gmx.com>
2022-12-07 15:48:42 -0800

    fix: bump version number from 2.1.6 -> 2.1.7

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -362,7 +362,7 @@ async def get_time():
 
 # @app.get("/agencies/")
 # async def root():
-#     return {"Metro API Version": "2.1.6"}
+#     return {"Metro API Version": "2.1.7"}
 
 # Frontend Routing
 
