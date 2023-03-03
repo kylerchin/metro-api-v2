10467301db6790989692e10b0d7f01079e693488
albertkun <albertk@gmx.com>
2023-03-02 16:55:55 -0800

    bump: versioning

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -408,7 +408,7 @@ async def get_time():
 
 # @app.get("/agencies/")
 # async def root():
-#     return {"Metro API Version": "2.1.16"}
+#     return {"Metro API Version": "2.1.16.1"}
 
 # Frontend Routing
 
