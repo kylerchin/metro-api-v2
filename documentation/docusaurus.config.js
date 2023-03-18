b14074aef5c5fa33d19f3a4ee8d09a3dcc1ed9a7
albertkun <albertk@gmx.com>
2023-03-17 18:37:39 -0700

    feat: initial documetation

---
+++ documentation/docusaurus.config.js
@@ -0,0 +1,175 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Metro API",
+  tagline: "Go Metro",
+  url: "https://api.metro.net",
+  baseUrl: "/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.png",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "LACMTA", // Usually your GitHub org/user name.
+  projectName: "metro-api-v2", // Usually your repo name.
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          docLayoutComponent: "@theme/DocPage",
+          docItemComponent: "@theme/ApiItem" // Derived from docusaurus-theme-openapi
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      })
+    ]
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      languageTabs: [
+        {
+          highlight: "bash",
+          language: "curl",
+        },
+        {
+          highlight: "python",
+          language: "python",
+        },
+        {
+          highlight: "go",
+          language: "go",
+        }],
+      docs: {
+        sidebar: {
+          hideable: true
+        }
+      },
+      navbar: {
+        title: "Metro API",
+        logo: {
+          alt: "Metro API Logo",
+          src: "img/favicon.png"
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Tutorial"
+          },
+          { to: "/blog", label: "Blog", position: "left" },
+          {
+            label: "API",
+            position: "left",
+            to: "/docs/api"
+          },
+          {
+            href: "https://github.com/facebook/docusaurus",
+            label: "GitHub",
+            position: "right"
+          }
+        ]
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/intro"
+              }
+            ]
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus"
+              },
+              {
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus"
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/docusaurus"
+              }
+            ]
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog"
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/facebook/docusaurus"
+              }
+            ]
+          }
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+
+      }
+    }),
+
+  plugins: [
+    [
+      "docusaurus-plugin-openapi-docs",
+      {
+        id: "openapi",
+        docsPluginId: "classic",
+        config: {
+          api: {
+            specPath: "https://api.metro.net/openapi.yaml",
+            outputDir: "docs/api",
+            baseUrl: "https://api.metro.net/",
+            downloadUrl:
+              "https://api.metro.net/openapi.yaml",
+            sidebarOptions: {
+              groupPathsBy: "tag",
+              categoryLinkSource: "tag"
+            }
+          }
+        }
+      }
+    ]
+  ],
+
+  themes: ["docusaurus-theme-openapi-docs"]
+};
+
+module.exports = config;
